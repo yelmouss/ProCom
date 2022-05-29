c0a487578f0fe340490afcf59d5b9884b5fcc33e
unknown <yelmouss.devt@gmail.com>
2022-05-29 20:09:41 +0100

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ProCom Network</title>
+    <!-- Le css Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
+
+    <script src="https://kit.fontawesome.com/f2e2773565.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light ">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img src="img/NB.png" class="img-fluid" alt="ProCom Logo"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="  justify-content-center" >
+            <ul class="navbar-nav justify-content-center p-5">
+                <li class="nav-item">
+                    <i class="fa-brands fa-twitter fa-xl m-4"></i>
+                </li>
+                <li class="nav-item">
+                    <i class="fa-brands fa-linkedin fa-xl m-4"></i>
+                </li>
+                <li class="nav-item">
+                    <i class="fa-brands fa-facebook fa-xl m-4"></i>
+                </li>
+
+                <li class="nav-item">
+                    <i class="fa-brands fa-youtube fa-xl m-4"></i>
+                </li>
+            </ul>
+            </div>
+            <div class="collapse navbar-collapse   justify-content-center" id="navbarSupportedContent">
+
+                <ul class="navbar-nav justify-content-center">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/">Accueil</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            NOUS CHOISIR
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">L’HUMAIN AU CŒUR DE LA RELATION CLIENT</a></li>
+                            <li><a class="dropdown-item" href="#">25 ANS AU SERVICE DES MARQUES</a></li>
+                            <li><a class="dropdown-item" href="#">NOS ÉQUIPES À TRAVERS LE MONDE</a></li>
+                            <li><a class="dropdown-item" href="#"> NOTRE ENGAGEMENT SOCIÉTAL</a></li>
+                            <li><a class="dropdown-item" href="#">NOS RÉFÉRENCES</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            NOTRE OFFRE
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#"> <i class="fa-brands fa-uncharted"></i> - CONSEIL & STRATÉGIE</a></li>
+                            <li><a class="dropdown-item" href="#"> <i class="fa-solid fa-hands-holding-child"></i> - GESTION DE LA RELATION CLIENT</a></li>
+                            <li><a class="dropdown-item" href="#"> <i class="fa-solid fa-hand-holding-dollar"></i> - CONNAISSANCE CLIENT</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            NOS SECTEURS D'ACTIVITÉS
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#"> <i class="fa-solid fa-plane-departure"></i> - AÉRIEN</a></li>
+                            <li><a class="dropdown-item" href="#"> <i class="fa-solid fa-chess"></i> - TOURISME</a></li>
+                            <li><a class="dropdown-item" href="#"> <i class="fa-solid fa-glasses"></i> - LUXE</a></li>
+                            <li><a class="dropdown-item" href="#"><i class="fa-solid fa-brain"></i> - CULTURE</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/">L'OBSERVATOIRE</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/">NOUS CONTACTER</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    </div>
+
+
+    <div class="container col-6">
+
+    </div>
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
